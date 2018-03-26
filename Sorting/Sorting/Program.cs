6b0ea1b430eb180afdf8fc88ed08ea3f56bd1e91
stevieb48 <seb48@students.uwf.edu>
2018-03-25 23:25:01 -0500

    Solution commit

---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * This class ...
+ *
+ * author: Group 1 (Louis Anthony, Tristan Barrett, Timothy Bates, Stephen Bailey)
+ * course: COP4365C
+ * assignment: Project 3
+ * date: 03/25/2018
+ * file name: ********.cs
+ * version: 1.0
+ */
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
